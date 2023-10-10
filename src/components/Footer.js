d9276bef7091ff49e12637aefb64ff1df1b23db0
georgswolo <georgielubberink@gmail.com>
2023-10-10 22:06:26 +1000

    react

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+
+    
+    
+    return (
+        <footer class="page-footer">
+                <a href="">task_alt</a>
+                <a href="">forum</a>
+                <a href="">home</a>
+                <a href="">leaderboard</a>
+                <a href="">person</a>
+        </footer>
+    )
+
+}
